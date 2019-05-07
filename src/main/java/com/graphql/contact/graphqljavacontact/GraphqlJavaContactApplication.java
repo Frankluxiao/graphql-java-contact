f45e66a5f2fd172e11a9733e0493d51f4cf64419
Xiao Lu <xiao.lu@zaplabs.com>
2019-05-07 10:07:36 -0700

    first commit

---
+++ src/main/java/com/graphql/contact/graphqljavacontact/GraphqlJavaContactApplication.java
@@ -0,0 +1,13 @@
+package com.graphql.contact.graphqljavacontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlJavaContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlJavaContactApplication.class, args);
+	}
+
+}
